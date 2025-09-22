d3dc4cc976060d04057e96a29aca1a84d8b266c7
Arghavan <arinkr7@gmail.com>
2025-09-22 16:21:47 +0330

    update article part 1 and 2

---
+++ src/app/(base)/article/page.tsx
@@ -0,0 +1,14 @@
+import SearchArticle from '@/components/Article/SearchArticle'
+import Slider from '@/components/Article/Slider'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <Slider/>
+      <SearchArticle/>
+    </div>
+  )
+}
+
+export default page
